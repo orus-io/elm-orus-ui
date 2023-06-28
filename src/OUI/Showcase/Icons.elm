ac1b5da697109457772ad3066bdf70bd26f35a46
Christophe de Vienne <christophe@cdevienne.info>
2023-06-28 11:36:29 +0200

    Icons showcase: better wrapping

--- src/OUI/Showcase/Icons.elm
+++ src/OUI/Showcase/Icons.elm
@@ -2,10 +2,12 @@ module OUI.Showcase.Icons exposing (book, withChapter)
 
 import Effect exposing (Effect)
 import Element exposing (Element)
+import Element.Border as Border
 import List.Extra as List
 import OUI.Explorer as Explorer
-import OUI.Icon exposing (Icon)
+import OUI.Icon as Icon exposing (Icon)
 import OUI.Material as Material
+import OUI.Material.Color
 import OUI.Material.Theme as Theme
 import OUI.Text as Text
 import OUI.TextField as TextField
@@ -16,25 +18,49 @@ withChapter title iconList =
     Explorer.withChapter <| iconChapter title iconList
 
 
+capitalize : String -> String
+capitalize =
+    String.split "_"
+        >> List.map
+            (\s ->
+                String.toUpper (String.left 1 s)
+                    ++ String.dropLeft 1 s
+            )
+        >> String.join " "
+
+
 iconChapter : String -> List ( String, Icon ) -> Explorer.Shared -> Model -> Element msg
 iconChapter title iconList shared model =
-    Element.column [ Element.spacing 10 ] <|
-        (Text.titleLarge title
+    Element.column [ Element.spacing 20, Element.paddingXY 20 0, Element.width <| Element.maximum 1200 <| Element.fill ] <|
+        [ Text.titleLarge title
             |> Material.text shared.theme
-        )
-            :: (iconList
-                    |> List.filter (\( label, _ ) -> String.contains model.filter label)
-                    |> List.map
-                        (\( label, icon ) ->
-                            Element.column [ Element.spacing 10 ]
-                                [ Material.icon shared.theme [ Element.centerX ] icon
-                                , Element.text label
-                                ]
-                        )
-                    |> List.greedyGroupsOf 10
-                    |> List.map
-                        (Element.row [ Element.spacing 20 ])
-               )
+        , iconList
+            |> List.filter (\( label, _ ) -> String.contains model.filter label)
+            |> List.map
+                (\( label, icon ) ->
+                    Element.column
+                        [ Element.spacing 25
+                        , Element.width <| Element.px 128
+                        , Element.height <| Element.px 128
+                        ]
+                        [ Material.icon shared.theme
+                            [ Element.centerX
+                            , Element.centerY
+                            ]
+                          <|
+                            Icon.withSize 40 icon
+                        , Element.el
+                            [ Element.centerX
+                            , Element.centerY
+                            ]
+                          <|
+                            Material.text shared.theme <|
+                                Text.bodySmall <|
+                                    capitalize label
+                        ]
+                )
+            |> Element.wrappedRow [ Element.centerX ]
+        ]
 
 
 filterChapter : Explorer.Shared -> Model -> Element (Explorer.BookMsg Msg)
@@ -43,6 +69,7 @@ filterChapter shared model =
         |> TextField.onFocusBlur (Explorer.bookMsg <| FilterFocus True) (Explorer.bookMsg <| FilterFocus False)
         |> TextField.withFocused model.filterFocused
         |> Material.textField shared.theme [ Element.width Element.fill ]
+        |> Element.el [ Element.padding 20, Element.width Element.fill ]
 
 
 book : String -> Explorer.Book Model Msg
