f376361080fb8ebd15e3a3341a4f149d79a1d5f6
Christophe de Vienne <christophe.devienne@orus.io>
2024-06-19 13:07:59 +0200

    Add a Modal API (from Orasund/elm-ui-widgets)

---
+++ src/OUI/Element.elm
@@ -0,0 +1,111 @@
+module OUI.Element exposing (Modal, singleModal, multiModal)
+
+{-| Utilities for Elm-UI
+
+
+# Modals
+
+This modal API is taken from
+[Orasund/elm-ui-widgets](https://package.elm-lang.org/packages/Orasund/elm-ui-widgets/latest/)
+
+@docs Modal, singleModal, multiModal
+
+-}
+
+import Element exposing (Attribute, Element)
+import Element.Background as Background
+import Element.Events as Events
+
+
+{-| -}
+type alias Modal msg =
+    { onDismiss : Maybe msg
+    , content : Element msg
+    }
+
+
+background : Maybe msg -> List (Attribute msg)
+background onDismiss =
+    [ Element.none
+        |> Element.el
+            ([ Element.width <| Element.fill
+             , Element.height <| Element.fill
+             , Background.color <| Element.rgba255 0 0 0 0.5
+             ]
+                ++ (onDismiss
+                        |> Maybe.map (Events.onClick >> List.singleton)
+                        |> Maybe.withDefault []
+                   )
+            )
+        |> Element.inFront
+    , Element.clip
+    ]
+
+
+{-| A modal showing a single element.
+
+Material design only allows one element at a time to be viewed as a modal.
+To make things easier, this widget only views the first element of the list.
+This way you can see the list as a queue of modals.
+
+    import Element
+
+    type Msg
+        = Close
+
+    Element.layout
+        (singleModal
+            [ { onDismiss = Just Close
+              , content =
+                  Element.text "Click outside this window to close it."
+              }
+            ]
+        )
+        |> always "Ignore this line" --> "Ignore this line"
+
+Technical Remark:
+
+  - To stop the screen from scrolling, set the height of the layout to the height of the screen.
+
+-}
+singleModal : List (Modal msg) -> List (Attribute msg)
+singleModal =
+    List.head
+        >> Maybe.map
+            (\{ onDismiss, content } ->
+                background onDismiss ++ [ content |> Element.inFront ]
+            )
+        >> Maybe.withDefault []
+
+
+{-| Same implementation as `singleModal` but also displays the "queued" modals.
+
+    import Element
+
+    type Msg
+        = Close
+
+    Element.layout
+        (multiModal
+            [ { onDismiss = Just Close
+              , content =
+                  Element.text "Click outside this window to close it."
+              }
+            ]
+        )
+        |> always "Ignore this line" --> "Ignore this line"
+
+-}
+multiModal : List (Modal msg) -> List (Attribute msg)
+multiModal list =
+    case list of
+        head :: tail ->
+            (tail
+                |> List.reverse
+                |> List.map (\{ content } -> content |> Element.inFront)
+            )
+                ++ background head.onDismiss
+                ++ [ head.content |> Element.inFront ]
+
+        _ ->
+            []
