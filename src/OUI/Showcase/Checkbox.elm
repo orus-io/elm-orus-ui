30ef9ab8c2849fa3b4ae3581c2ab76ee3d0634e5
Axel PREL <axel.prel@xcg-consulting.fr>
2024-07-04 10:37:10 +0200

    beautify checkbox showcase

--- src/OUI/Showcase/Checkbox.elm
+++ src/OUI/Showcase/Checkbox.elm
@@ -3,9 +3,11 @@ module OUI.Showcase.Checkbox exposing (book)
 import Element exposing (Element)
 import OUI
 import OUI.Checkbox as Checkbox
+import OUI.Divider as Divider
 import OUI.Explorer as Explorer
 import OUI.Icon exposing (clear)
 import OUI.Material as Material
+import OUI.Text as Text
 
 
 book : Explorer.Book themeExt () ()
@@ -29,39 +31,61 @@ onChange name checked =
 
 checkbox : Explorer.Shared themeExt -> Element (Explorer.BookMsg ())
 checkbox { theme } =
+    let
+        divider =
+            Divider.new |> Material.divider theme []
+    in
     Element.column [ Element.spacing 30 ]
-        [ Element.text "Checkbox"
-        , Element.row [ Element.spacing 30 ]
-            [ Checkbox.new
-                |> Checkbox.onChange (onChange "unchecked")
-                |> Checkbox.withChecked False
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.onChange (onChange "checked")
-                |> Checkbox.withChecked True
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.disabled
-                |> Checkbox.withChecked False
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.disabled
-                |> Checkbox.withChecked True
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.onChange (onChange "custom icon")
-                |> Checkbox.withChecked True
-                |> Checkbox.withIcon clear
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.onChange (onChange "unchecked error")
-                |> Checkbox.withChecked False
-                |> Checkbox.withColor OUI.Error
-                |> Material.checkbox theme []
-            , Checkbox.new
-                |> Checkbox.onChange (onChange "checked error")
-                |> Checkbox.withChecked True
-                |> Checkbox.withColor OUI.Error
-                |> Material.checkbox theme []
+        [ divider
+        , Element.row [ Element.spacing 30, Element.padding 30 ]
+            [ Element.column [ Element.spacing 55, Element.padding 30 ]
+                [ Element.none |> Element.el []
+                , Text.titleSmall "Basic" |> Material.text theme
+                , Text.titleSmall "Disabled" |> Material.text theme
+                , Text.titleSmall "Custom Icon" |> Material.text theme
+                , Text.titleSmall "Error" |> Material.text theme
+                ]
+            , Element.column [ Element.spacing 30 ]
+                [ Text.titleSmall "Unchecked" |> Material.text theme
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "unchecked")
+                    |> Checkbox.withChecked False
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.disabled
+                    |> Checkbox.withChecked False
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "custom icon unchecked")
+                    |> Checkbox.withChecked False
+                    |> Checkbox.withIcon clear
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "unchecked error")
+                    |> Checkbox.withChecked False
+                    |> Checkbox.withColor OUI.Error
+                    |> Material.checkbox theme []
+                ]
+            , Element.column [ Element.spacing 30 ]
+                [ Text.titleSmall "Checked" |> Material.text theme
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "checked")
+                    |> Checkbox.withChecked True
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.disabled
+                    |> Checkbox.withChecked True
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "custom icon checked")
+                    |> Checkbox.withChecked True
+                    |> Checkbox.withIcon clear
+                    |> Material.checkbox theme []
+                , Checkbox.new
+                    |> Checkbox.onChange (onChange "checked error")
+                    |> Checkbox.withChecked True
+                    |> Checkbox.withColor OUI.Error
+                    |> Material.checkbox theme []
+                ]
             ]
         ]
