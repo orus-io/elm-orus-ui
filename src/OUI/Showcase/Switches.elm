a20afbe7007f0215fec761494f144e2f9ab43b90
Axel PREL <axel.prel@xcg-consulting.fr>
2024-07-04 11:39:28 +0200

    beautify switch showcase

--- src/OUI/Showcase/Switches.elm
+++ src/OUI/Showcase/Switches.elm
@@ -4,15 +4,17 @@ import Dict exposing (Dict)
 import Effect
 import Element exposing (Element)
 import OUI
+import OUI.Divider as Divider
 import OUI.Explorer as Explorer
 import OUI.Icon exposing (check, clear)
 import OUI.Material as Material
 import OUI.Switch as Switch
+import OUI.Text as Text
 
 
 book : Explorer.Book themeExt Model Msg
 book =
-    Explorer.statefulBook "Switch"
+    Explorer.statefulBook "Switches"
         { init =
             \_ ->
                 { switches = Dict.empty }
@@ -39,21 +41,26 @@ type Msg
 
 checkbox : Explorer.Shared themeExt -> Model -> Element (Explorer.BookMsg Msg)
 checkbox { theme } { switches } =
+    let
+        divider : Element msg
+        divider =
+            Divider.new |> Material.divider theme []
+    in
     Element.column [ Element.spacing 30, Element.width <| Element.px 300 ]
-        [ Element.text "Switches"
+        [ divider
         , Element.row [ Element.spacing 30, Element.width Element.fill ]
-            [ Element.text "Primary color"
+            [ Text.titleMedium "Primary color" |> Material.text theme
             , Switch.new (Dict.get "primary" switches |> Maybe.withDefault True)
                 |> Switch.onChange (Explorer.bookMsg << SetSwitch "primary")
                 |> Material.switch theme [ Element.alignRight ]
             ]
         , Element.row [ Element.spacing 30, Element.width Element.fill ]
-            [ Element.text "Disabled"
+            [ Text.titleMedium "Disabled" |> Material.text theme
             , Switch.new (Dict.get "primary" switches |> Maybe.withDefault True)
                 |> Material.switch theme [ Element.alignRight ]
             ]
         , Element.row [ Element.spacing 30, Element.width Element.fill ]
-            [ Element.text "With icons"
+            [ Text.titleMedium "With icon" |> Material.text theme
             , Switch.new (Dict.get "icons" switches |> Maybe.withDefault True)
                 |> Switch.onChange (Explorer.bookMsg << SetSwitch "icons")
                 |> Switch.withIconUnselected clear
@@ -61,14 +68,14 @@ checkbox { theme } { switches } =
                 |> Material.switch theme [ Element.alignRight ]
             ]
         , Element.row [ Element.spacing 30, Element.width Element.fill ]
-            [ Element.text "Secondary color"
+            [ Text.titleMedium "Secondary color" |> Material.text theme
             , Switch.new (Dict.get "secondary" switches |> Maybe.withDefault True)
                 |> Switch.withColor OUI.Secondary
                 |> Switch.onChange (Explorer.bookMsg << SetSwitch "secondary")
                 |> Material.switch theme [ Element.alignRight ]
             ]
         , Element.row [ Element.spacing 30, Element.width Element.fill ]
-            [ Element.text "Error color"
+            [ Text.titleMedium "Error color" |> Material.text theme
             , Switch.new (Dict.get "error" switches |> Maybe.withDefault True)
                 |> Switch.onChange (Explorer.bookMsg << SetSwitch "error")
                 |> Switch.withColor OUI.Error
