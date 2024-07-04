0040f5656fc2efca7208c47bae6d4b6e3d33fe08
Axel PREL <axel.prel@xcg-consulting.fr>
2024-07-04 12:21:15 +0200

    beautify badge showcase

--- src/OUI/Showcase/Badge.elm
+++ src/OUI/Showcase/Badge.elm
@@ -2,10 +2,11 @@ module OUI.Showcase.Badge exposing (book)
 
 import Element exposing (Element)
 import OUI.Badge
+import OUI.Divider as Divider
 import OUI.Explorer as Explorer
 import OUI.Icon
-import OUI.Material
-import OUI.Text
+import OUI.Material as Material
+import OUI.Text as Text
 
 
 book : Explorer.Book themeExt () ()
@@ -15,44 +16,52 @@ book =
 
 
 badges : Explorer.Shared themeExt -> Element msg
-badges shared =
-    Element.table
-        [ Element.spacing 20 ]
-        { data =
-            [ { text = "small"
-              , badge = OUI.Badge.small
-              }
-            , { text = "with text"
-              , badge = OUI.Badge.label "new"
-              }
-            , { text = "with large text"
-              , badge = OUI.Badge.label "Lorem Ipsus"
-              }
-            , { text = "with number"
-              , badge = OUI.Badge.number 10
-              }
-            , { text = "with large number"
-              , badge = OUI.Badge.number 12348
-              }
-            ]
-        , columns =
-            [ { header = Element.none
-              , width = Element.shrink
-              , view =
-                    .text
-                        >> OUI.Text.bodyLarge
-                        >> OUI.Material.text shared.theme
-              }
-            , { header = Element.none
-              , width = Element.shrink
-              , view =
-                    \{ badge } ->
-                        OUI.Icon.light_mode
-                            |> OUI.Material.icon shared.theme
-                                [ badge
-                                    |> OUI.Material.badge shared.theme []
-                                ]
-                            |> Element.el []
-              }
-            ]
-        }
+badges { theme } =
+    let
+        divider : Element msg
+        divider =
+            Divider.new |> Material.divider theme []
+    in
+    Element.column [ Element.spacing 30 ]
+        [ divider
+        , Element.table
+            [ Element.spacing 20 ]
+            { data =
+                [ { text = "small"
+                  , badge = OUI.Badge.small
+                  }
+                , { text = "with text"
+                  , badge = OUI.Badge.label "new"
+                  }
+                , { text = "with large text"
+                  , badge = OUI.Badge.label "Lorem Ipsum"
+                  }
+                , { text = "with number"
+                  , badge = OUI.Badge.number 10
+                  }
+                , { text = "with large number"
+                  , badge = OUI.Badge.number 12348
+                  }
+                ]
+            , columns =
+                [ { header = Element.none
+                  , width = Element.shrink
+                  , view =
+                        .text
+                            >> Text.titleSmall
+                            >> Material.text theme
+                  }
+                , { header = Element.none
+                  , width = Element.shrink
+                  , view =
+                        \{ badge } ->
+                            OUI.Icon.light_mode
+                                |> Material.icon theme
+                                    [ badge
+                                        |> Material.badge theme []
+                                    ]
+                                |> Element.el []
+                  }
+                ]
+            }
+        ]
